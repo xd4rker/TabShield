dcfd5e9d3cef39eb088d0e0a7f010b455e71f2cd
Ismail <xd4rker@users.noreply.github.com>
2025-03-12 00:03:22 +0100

    chore: format codebase (#17)

--- src/common/utils/domUtils.spec.ts
+++ src/common/utils/domUtils.spec.ts
@@ -1,28 +1,30 @@
 /** @jest-environment jsdom */
 
-import { DomUtils } from "./domUtils";
+import { DomUtils } from './domUtils';
 
-describe("DomUtils", () => {
-    let element: HTMLDivElement;
+describe('DomUtils', () => {
+  let element: HTMLDivElement;
 
-    beforeEach(() => {
-        element = document.createElement("div");
-        element.id = "test-element";
-        document.body.appendChild(element);
-    });
+  beforeEach(() => {
+    element = document.createElement('div');
+    element.id = 'test-element';
+    document.body.appendChild(element);
+  });
 
-    afterEach(() => {
-        document.body.removeChild(element);
-    });
+  afterEach(() => {
+    document.body.removeChild(element);
+  });
 
-    test("should retrieve an existing element by ID", () => {
-        const retrievedElement = DomUtils.getElement<HTMLDivElement>("test-element");
-        expect(retrievedElement).toBeInstanceOf(HTMLDivElement);
-        expect(retrievedElement.id).toBe("test-element");
-    });
+  test('should retrieve an existing element by ID', () => {
+    const retrievedElement =
+      DomUtils.getElement<HTMLDivElement>('test-element');
+    expect(retrievedElement).toBeInstanceOf(HTMLDivElement);
+    expect(retrievedElement.id).toBe('test-element');
+  });
 
-    test("should throw an error when element does not exist", () => {
-        expect(() => DomUtils.getElement<HTMLDivElement>("non-existent"))
-            .toThrow("Element with ID 'non-existent' not found");
-    });
+  test('should throw an error when element does not exist', () => {
+    expect(() => DomUtils.getElement<HTMLDivElement>('non-existent')).toThrow(
+      "Element with ID 'non-existent' not found"
+    );
+  });
 });
--- src/common/utils/urlUtils.ts
+++ src/common/utils/urlUtils.ts
@@ -1,19 +1,21 @@
 export class UrlUtils {
-    /**
-    * Returns whether a given URL is special
-    */
-    static isSpecialUrl(url: string): boolean {
-        return /^(about:|chrome:\/\/|edge:\/\/|opera:\/\/|brave:\/\/|safari-resource:\/\/|chrome-extension:|moz-extension:)/.test(url);
-    }
+  /**
+   * Returns whether a given URL is special
+   */
+  static isSpecialUrl(url: string): boolean {
+    return /^(about:|chrome:\/\/|edge:\/\/|opera:\/\/|brave:\/\/|safari-resource:\/\/|chrome-extension:|moz-extension:)/.test(
+      url
+    );
+  }
 
-    /**
-     * Get hostname from URL
-     */
-    static getHostname(url: string): string | null {
-        try {
-            return new URL(url).hostname;
-        } catch {
-            return null;
-        }
+  /**
+   * Get hostname from URL
+   */
+  static getHostname(url: string): string | null {
+    try {
+      return new URL(url).hostname;
+    } catch {
+      return null;
     }
+  }
 }
